c6c52c258f5d2e92747849aa2affcf2b35218cb8
Ilya Osipov <il.osipov.gm@gmail.com>
2020-11-18 14:21:18 +0300

    upgrade navigation components

--- app/src/main/java/com/ilosipov/news_app/MainActivity.kt
+++ app/src/main/java/com/ilosipov/news_app/MainActivity.kt
@@ -2,10 +2,28 @@ package com.ilosipov.news_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ilosipov.news_app.databinding.ActivityMainBinding
+
+/**
+ * Class MainActivity
+ * @author Ilya Osipov (mailto:il.osipov.gm@gmail.com)
+ * @since 18.11.2020
+ * @version $Id$
+ */
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
