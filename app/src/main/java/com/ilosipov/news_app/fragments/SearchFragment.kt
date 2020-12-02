eb457dbd686c68cfa333c18854900b7331f3ec39
Ilya Osipov <il.osipov.gm@gmail.com>
2020-12-02 22:49:08 +0300

    added view search fragment

--- app/src/main/java/com/ilosipov/news_app/fragments/SearchFragment.kt
+++ app/src/main/java/com/ilosipov/news_app/fragments/SearchFragment.kt
@@ -1,12 +1,15 @@
 package com.ilosipov.news_app.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.ilosipov.news_app.R
+import com.ilosipov.news_app.adapters.news.NewsAdapter
+import com.ilosipov.news_app.data.NewsItem
 import com.ilosipov.news_app.databinding.FragmentSearchBinding
 
 /**
@@ -19,11 +22,33 @@ import com.ilosipov.news_app.databinding.FragmentSearchBinding
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var adapterNews: NewsAdapter
+    private lateinit var fakeSearchNewsList: List<NewsItem>
+
+    companion object {
+        private const val TAG = "SearchFragment"
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+        Log.i(TAG, "onCreateView: start fragment")
         binding = DataBindingUtil.inflate(LayoutInflater.from(requireContext()),
             R.layout.fragment_search, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i(TAG, "onViewCreated: init view")
+        fakeSearchNewsList = emptyList()
+
+        checkList(fakeSearchNewsList.isEmpty())
+    }
+
+    private fun checkList(isEmpty: Boolean) {
+        binding.apply {
+            rvSearchNews.visibility = if (isEmpty) View.GONE else View.VISIBLE
+            textEmptyList.visibility = if (isEmpty) View.VISIBLE else View.GONE
+        }
     }
 }
